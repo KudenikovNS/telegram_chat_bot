f66c3abb91ecb27e386875369ae12c2ae3b5d227
Nikita Kudenikov <KudenikovNS@yandex.ru>
2023-03-28 12:42:56 +0400

    First commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import openai
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+token = ''
+openai.api_key = ''
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer("Добрый день! Вы можете задать мне вопрос.")
+
+@dp.message_handler()
+async def send(message: types.Message):
+    try:
+        if not message.text.endswith("?"):
+            message.text += "?"
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=message.text,
+            temperature=0.9,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.6,
+            stop=["You:"]
+        )
+        await message.answer(response['choices'][0]['text'])
+    except Exception as e:
+        print("An error occurred while processing the message: ", e)
+        await message.answer("Произошла ошибка при обработке вашего запроса. Попробуйте еще раз позднее.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
